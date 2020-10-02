27857e9f9070895d28d726b51e3ed6c7f0940c1d
RyanVreeke <rvreeke0229@gmail.com>
2020-10-02 14:20:14 -0400

    Added more format to the groups stack.

--- screens/groups.js
+++ screens/groups.js
@@ -9,9 +9,66 @@ import {
 import { globalStyles } from "../styles/global";
 
 export default function Groups({ navigation }) {
+  const [groups, setGroups] = useState([
+    { name: "example 1", key: "1" },
+    { name: "example 2", key: "2" },
+  ]);
+
   return (
-    <View>
-      <Text>Groups screen</Text>
+    <View style={globalStyles.container}>
+      <View style={styles.groupsContainer}>
+        <View style={styles.listContainer}>
+          <FlatList
+            data={groups}
+            renderItem={({ item }) => (
+              <TouchableOpacity>
+                <View style={styles.groupContainer}>
+                  <Text style={styles.groupNames}>{item.name}</Text>
+                </View>
+              </TouchableOpacity>
+            )}
+          />
+        </View>
+      </View>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  groupsContainer: {
+    backgroundColor: "white",
+    borderColor: "#00AAFF",
+    borderWidth: 1.5,
+    borderRadius: 5,
+    margin: 0,
+    padding: 10,
+    width: "43%",
+    height: "100%",
+  },
+
+  listContainer: {
+    flex: 1,
+  },
+
+  groupContainer: {
+    flex: 1,
+    justifyContent: "space-around",
+    backgroundColor: "white",
+    borderColor: "#00AAFF",
+    borderWidth: 1.5,
+    borderRadius: 5,
+    elevation: 3,
+    shadowOffset: { width: 1, height: 1 },
+    shadowColor: "#333",
+    shadowOpacity: 0.3,
+    shadowRadius: 2,
+    margin: 3,
+    paddingTop: 19,
+    paddingBottom: 19,
+  },
+
+  groupNames: {
+    color: "black",
+    textAlign: "center",
+  },
+});
