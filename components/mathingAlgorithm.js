5fde92fa3f1d8b743c753a5dbfb0690450b3d6cb
David <31216720+Agnarr43@users.noreply.github.com>
2020-10-08 12:03:34 -0400

    moved the file and fixed a bug

--- components/mathingAlgorithm.js
+++ components/mathingAlgorithm.js
@@ -4,7 +4,7 @@ import { globalStyles } from "../styles/global";
 
 const user1 = {
     name: "David Sen",
-    sun: [0,0,1,0,1,0,0,1,1,1,1],
+    sun: [0,1,1,0,1,0,0,1,1,1,1],
     mon: [],
     tues: [],
     wed: [],
@@ -30,32 +30,31 @@ const matchTimes = (userList) => {
     const baseLine = [0,0,0,0,0,0,0,0,0,0,0];
     const dayKeys = ["sun","mon","tues","wed","thurs","fri","sat"];
     const matchedTimes = [ [], [], [], [], [], [], [] ];
-    for (n=0; n<7; n++) { //Each day of the week
-        for (i = 1; i < userList.length; i++) { //Each user - looks at pairs of users so starts at 2nd user
-            for (x=0; x < userList[0].sun.length; x++) { //Each timeslot in a day
-                matchedTimes[n][x] = userList[i-1][dayKeys[n]][x] && userList[i][dayKeys[n]][x]; //check if the and works
+    for (let n=0; n<7; n++) { //Each day of the week
+        for (let i = 1; i < userList.length; i++) { //Each user - looks at pairs of users so starts at 2nd user
+            for (let x=0; x < userList[0].sun.length; x++) { //Each timeslot in a day
+                if ( i === 1) {
+                    matchedTimes[n][x] = userList[i-1][dayKeys[n]][x] && userList[i][dayKeys[n]][x]; //Right now matchedTimes[n] is empty, so compare 2 users
+                } else {
+                    matchedTimes[n][x] = matchedTimes[n][x] && userList[i][dayKeys[n]][x]; //Right now it's not empty, so compare user to it
+                }
             }
         }
     }
+    return(matchedTimes);
     // Still TODO
     // Heatmap, keep track of names
     // format output
     // map truthy indexes to times (either in this function or make another function for that, use a calculation not just index time matching)
 };
 //const result = matchTimes([user1, user2]);
+
 export default function matchingAlgorithm({ navigation }) {
   const [weekDays, setWeekDays] = useState([
   ]);
-
+  
   return (
     <View style={globalStyles.container}>
-        <TouchableOpacity
-            onPress={() => navigation.navigate("UserTimes", item)}
-            >
-            <View style={styles.dayContainer}>
-                <Text style={styles.days}>{item.day}</Text>
-            </View>
-        </TouchableOpacity>
     </View>
   );
 }
