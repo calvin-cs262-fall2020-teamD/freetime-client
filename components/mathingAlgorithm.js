b6557aba53f150fbd44b4e5f4601a9b5d6cc831d
David <31216720+Agnarr43@users.noreply.github.com>
2020-10-08 23:08:11 -0400

    began to improve algorithm

--- components/mathingAlgorithm.js
+++ components/mathingAlgorithm.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import {View, StyleSheet, Text, FlatList, TouchableOpacity, } from "react-native";
 import { globalStyles } from "../styles/global";
-
+import * as math from "mathjs";
 const user1 = {
     name: "David Sen",
     sun: [0,1,1,0,1,0,0,1,1,1,1],
@@ -27,7 +27,6 @@ const matchTimes = (userList) => {
     //Things we need this to do
     // - Find a time everybody is free 
     // - I really like the heatmap feature of when2meet.com
-    const baseLine = [0,0,0,0,0,0,0,0,0,0,0];
     const dayKeys = ["sun","mon","tues","wed","thurs","fri","sat"];
     const matchedTimes = [ [], [], [], [], [], [], [] ];
     for (let n=0; n<7; n++) { //Each day of the week
@@ -42,12 +41,65 @@ const matchTimes = (userList) => {
         }
     }
     return(matchedTimes);
-    // Still TODO
-    // Heatmap, keep track of names
-    // format output
-    // map truthy indexes to times (either in this function or make another function for that, use a calculation not just index time matching)
+    //Notes
+    //Instead, add up total # of people with that free time in a matrix
+    //Attatch their names to respective cells
+        //Have 2 identically sized matricies, one having triple nested arrays containing a person list
+    //When it's filled go through and analyze, grouping times with the same # of people
+    //
 };
 //const result = matchTimes([user1, user2]);
+
+const initialMatchTimes = (userList) => {
+    //For this we'd all need to install math.js
+    //const matrix = math.zeros(7,userList[0].sun.length,[]); //[rows][cols]
+
+    //NEW PLAN! Initialize Array
+    const numberTimeIncrements = userList[0].sun.length;
+    const matrix = [ [], [], [], [], [], [], [] ];
+    for (let b  = 0; b < numberTimeIncrements; b++) {
+        for (let m = 0; m < 7; m++) {
+            matrix[m][b] = [];
+          }
+    }
+    
+
+    const dayKeys = ["sun","mon","tues","wed","thurs","fri","sat"];
+    for (let n=0; n<7; n++) { //Each day of the week
+        for (let i = 0; i < userList.length; i++) { //Each user
+            for (let x=0; x < numberTimeIncrements; x++) { //Each timeslot in a day
+                if ( i === userList.length - 1) { //On the last user, while we're already going through, keep track of the best times
+                    matrix[n][x][ matrix[n][x].length ] = userList[i].name;
+                    
+                } else {
+                    matrix[n][x][ matrix[n][x].length ] = userList[i].name;
+                }
+
+            }
+        }
+    }
+
+}
+
+const updateTimes = (user) => {
+    //updates the matrix based purely on 1 additional user
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 export default function matchingAlgorithm({ navigation }) {
   const [weekDays, setWeekDays] = useState([
