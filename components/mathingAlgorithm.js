8d7d7c5d60cbf3efa7a5acdd341a26ac1fd4dbaa
David <31216720+Agnarr43@users.noreply.github.com>
2020-10-13 21:05:53 -0400

    some consolidation and continued work

--- components/mathingAlgorithm.js
+++ components/mathingAlgorithm.js
@@ -23,38 +23,9 @@ const user2 = {
     fri: [],
     sat: [],
 };
+
 const matchTimes = (userList) => {
-    //Things we need this to do
-    // - Find a time everybody is free 
-    // - I really like the heatmap feature of when2meet.com
-    const dayKeys = ["sun","mon","tues","wed","thurs","fri","sat"];
-    const matchedTimes = [ [], [], [], [], [], [], [] ];
-    for (let n=0; n<7; n++) { //Each day of the week
-        for (let i = 1; i < userList.length; i++) { //Each user - looks at pairs of users so starts at 2nd user
-            for (let x=0; x < userList[0].sun.length; x++) { //Each timeslot in a day
-                if ( i === 1) {
-                    matchedTimes[n][x] = userList[i-1][dayKeys[n]][x] && userList[i][dayKeys[n]][x]; //Right now matchedTimes[n] is empty, so compare 2 users
-                } else {
-                    matchedTimes[n][x] = matchedTimes[n][x] && userList[i][dayKeys[n]][x]; //Right now it's not empty, so compare user to it
-                }
-            }
-        }
-    }
-    return(matchedTimes);
-    //Notes
-    //Instead, add up total # of people with that free time in a matrix
-    //Attatch their names to respective cells
-        //Have 2 identically sized matricies, one having triple nested arrays containing a person list
-    //When it's filled go through and analyze, grouping times with the same # of people
-    //
-};
-//const result = matchTimes([user1, user2]);
-
-const initialMatchTimes = (userList) => {
-    //For this we'd all need to install math.js
-    //const matrix = math.zeros(7,userList[0].sun.length,[]); //[rows][cols]
-
-    //NEW PLAN! Initialize Array
+    //Initialize Array
     const numberTimeIncrements = userList[0].sun.length;
     const matrix = [ [], [], [], [], [], [], [] ];
     for (let b  = 0; b < numberTimeIncrements; b++) {
@@ -70,7 +41,7 @@ const initialMatchTimes = (userList) => {
             for (let x=0; x < numberTimeIncrements; x++) { //Each timeslot in a day
                 if ( i === userList.length - 1) { //On the last user, while we're already going through, keep track of the best times
                     matrix[n][x][ matrix[n][x].length ] = userList[i].name;
-                    
+                    //TODO log best slots?
                 } else {
                     matrix[n][x][ matrix[n][x].length ] = userList[i].name;
                 }
