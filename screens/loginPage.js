b0f4be964b3653d4abeaf34a42ce5d3265bf2d93
David <31216720+Agnarr43@users.noreply.github.com>
2020-10-22 23:10:16 -0400

    added file

---
+++ screens/loginPage.js
@@ -0,0 +1,18 @@
+import React, { useState } from "react";
+import {
+  View,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+} from "react-native";
+import { globalStyles } from "../styles/global";
+
+export default function Login() {
+  return (
+    <View style={globalStyles.container}>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+});
