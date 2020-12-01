0d45b6e5959fa0a29f0a786cff1539d1f1af6c90
Caleb Hurshman <caleb@hurshman.com>
2020-11-30 20:32:01 -0500

    updated group screen UI, moved common styles to globalStyles

--- screens/userWeek.js
+++ screens/userWeek.js
@@ -16,16 +16,16 @@ export default function UserWeek({navigation}) {
 
     return (
         <View style={globalStyles.container}>
-            <View style={styles.listContainer}>
+            <View style={globalStyles.listContainer}>
                 <FlatList
                     data={context.weekDays}
                     renderItem={({item}) => (
-                        <View style={styles.weekDayHeader}>
+                        <View style={globalStyles.moduleHeader}>
                             <TouchableOpacity
                                 onPress={() => navigation.navigate("UserTimes", item)}
                             >
-                                <Text style={styles.weekDayHeaderText}>{item.day}</Text>
-                                <View style={styles.weekDayBox}>
+                                <Text style={globalStyles.moduleHeaderText}>{item.day}</Text>
+                                <View style={globalStyles.moduleBox}>
                                     <Text></Text>
                                 </View>
                             </TouchableOpacity>
@@ -38,59 +38,3 @@ export default function UserWeek({navigation}) {
         </View>
     );
 }
-
-const styles = StyleSheet.create({
-    userWeekContainer: {
-        backgroundColor: "white",
-        borderColor: "#70cefa",
-        borderWidth: 1.5,
-        borderRadius: 5,
-        marginBottom: 5,
-        padding: '3%',
-        width: "50%",
-        height: "99%",
-    },
-
-    listContainer: {
-        flex: 1,
-        width: "90%",
-        alignSelf: "center",
-    },
-
-    weekDayHeader: {
-        flex: 1,
-        justifyContent: "space-around",
-        backgroundColor: "#70cefa",
-        borderColor: "black",
-        //borderBottomColor: "#70cefa",
-        borderWidth: 1.5,
-        borderRadius: 5,
-        elevation: 5,
-        shadowOffset: {width: -.5, height: .5},
-        shadowColor: "#333",
-        shadowOpacity: 0.3,
-        shadowRadius: 3,
-        margin: 3,
-        marginTop: 30,
-        paddingTop: 19,
-    },
-
-    weekDayHeaderText: {
-        alignSelf: "center",
-        fontSize: 20
-    },
-
-    weekDayBox: {
-        flex: 1,
-        backgroundColor: "#EEE",
-        borderWidth: 1.5,
-        borderRadius: 5,
-        borderColor: "#70cefa",
-        borderBottomColor: "transparent",
-        elevation: 5,
-        margin: 10,
-        marginBottom: 1,
-        height: 125,
-
-    }
-});
